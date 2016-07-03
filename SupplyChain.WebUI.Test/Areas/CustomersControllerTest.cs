74877131c9d088936773acf9444a775f88ad5f24
randelramirez <randelramirez1@gmail.com>
2016-07-03 21:43:11 +0800

    added test

---
+++ SupplyChain.WebUI.Test/Areas/CustomersControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupplyChain.WebUI.Areas.BusinessPartners.Controllers;
+
+namespace SupplyChain.WebUI.Test.Areas
+{
+    [TestClass]
+    public class CustomersControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var controller = new CustomersController();
+            //controller as 
+        }
+    }
+}
